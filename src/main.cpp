d83d6064f30a3c1209cf002f003e785e1f7471b5
Илья Шибанов <shia@pbprog.ru>
2024-04-04 22:53:49 +0300

    CMake и Hello World

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+
+#pragma warning(push)
+#pragma warning(disable: 6031)
+
+int main (int argc, char* argv[])
+{
+    std::cout << "Hello World" << std::endl;
+    _getch();
+    return 0;
+}
