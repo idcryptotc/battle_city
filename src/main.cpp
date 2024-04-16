b88ed29cdb4b875f671b146c7f8012bb291b1a27
Илья Шибанов <shia@pbprog.ru>
2024-04-16 22:07:54 +0300

    Подключена библиотека GLFW; Добавлен пример для теста с сайта

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
-#include <conio.h>
+#include <GLFW/glfw3.h>
 
-#pragma warning(push)
-#pragma warning(disable: 6031)
-
-int main (int argc, char* argv[])
+int main(void)
 {
-    std::cout << "Hello World" << std::endl;
-    _getch();
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
