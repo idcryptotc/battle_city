e7e39d2d5b360ffb28e28c5e49c016fe1c2ba812
fanateek <idcryptotc@yandex.ru>
2025-01-22 23:33:12 +0300

    11 - OpenGL - 04 - менеджер ресурсов

--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <glad/glad.h>
-#include<string>
+#include <string>
 
 namespace Renderer
 {
---
+++ src/Resources/ResourceManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <map>
+
+namespace Renderer
+{
+	class ShaderProgram;
+}
+
+class ResourceManager
+{
+public:
+	ResourceManager(const ResourceManager&) = delete;
+	ResourceManager(const ResourceManager&&) = delete;
+	ResourceManager(const std::string& executablePath);
+
+	~ResourceManager() = default;
+
+	ResourceManager& operator=(const ResourceManager&) = delete;
+	ResourceManager& operator=(ResourceManager&&) = delete;
+
+	std::shared_ptr<Renderer::ShaderProgram> loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath);
+	std::shared_ptr<Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
+
+private:
+	std::string getFileString(const std::string& relativeFilePath) const;
+
+	typedef std::map<const std::string, std::shared_ptr<Renderer::ShaderProgram>> ShaderProgramsMap;
+	ShaderProgramsMap m_shaderProgramsMap;
+
+	std::string m_path;
+};
